a1eb34c8976b6b732cb605172dda26e301f67cb7
Nico Nijenhuis <nico.nijenhuis@gmail.com>
2020-01-19 11:24:04 +0100

    Updated log.py

--- log.py
+++ log.py
@@ -1,14 +1,21 @@
 # logspul
 
-import logging
-logger = logging.getLogger('myLog')
-#logging.basicConfig(level=logging.DEBUG)
-#logger.setLevel(logging.DEBUG)
 
 def bla():
     return 'bla'
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    from argparse import ArgumentParser
+    import logging
+    parser = ArgumentParser(description='Logtool')
+    parser.add_argument('-ll', '--loglevel', type=str, choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Set the logging level')
+    args = parser.parse_args()
+
+    logger = logging.getLogger('myLog')
+    logging.basicConfig(level=args.loglevel)
+    #logger.setLevel(logging.DEBUG)
+    #logging.basicConfig(level=logging.DEBUG)
     x = bla()
     logger.debug('Dit wordt niet wat')
+    logger.info('Hier verwacht ik niets van')
+    logger.warning('Dit gaat helemaal niet goed')
